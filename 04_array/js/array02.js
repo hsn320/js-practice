f622739be5f209cef4777455079e9690bb429541
hsn320 <2250025@ecc.ac.jp>
2025-09-10 02:10:43 +0900

    array02

---
+++ 04_array/js/array02.js
@@ -0,0 +1,26 @@
+const arr = [];
+
+//arrは配列名 []のことを要素 0のことを要素番号といいます
+arr[0] = "windows OS";
+arr[1] = "Mac OS";
+arr[2] = "Linux OS";
+
+console.log("OSは全部で" + arr.length + "つあります。");
+
+const ramen = [];
+
+ramen[0] = "sio";
+ramen[1] = "miso";
+ramen[2] = "tonkotu";
+ramen[3] = "nibosi";
+ramen[4] = "shoyu";
+ramen[5] = "jirou";
+
+console.log("ここのお店のラーメンは全部で" + ramen.length + "種類、おすすめは" + ramen[1] + "です")
+
+console.log(ramen[0]);
+console.log(ramen[1]);
+console.log(ramen[2]);
+console.log(ramen[3]);
+console.log(ramen[4]);
+console.log(ramen[5]);
