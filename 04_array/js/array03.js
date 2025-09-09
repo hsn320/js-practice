4fdc81f39f6b109c007c39b28621ea46eca1f8d9
hsn320 <2250025@ecc.ac.jp>
2025-09-10 02:16:38 +0900

    array03

---
+++ 04_array/js/array03.js
@@ -0,0 +1,13 @@
+let adobe = ["Photoshop","Illustrator"];
+//配列を表示
+console.log(adobe);
+//要素の最後にXdを追加
+adobe.push("Xd");
+//配列を表示
+console.log(adobe);
+
+let web = ["WD1A","WD2A"];
+console.log(web);
+
+web.push("WD3A");
+console.log(web);
