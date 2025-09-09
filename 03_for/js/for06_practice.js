62a14c06cc4d87b07b425ff54e011c075151bead
hsn320 <2250025@ecc.ac.jp>
2025-09-10 01:05:28 +0900

    for06

--- 03_for/js/for06_practice.js
+++ 03_for/js/for06_practice.js
@@ -2,3 +2,19 @@
 let wallet = 12800;
 //価格
 let price = 980;
+let count = 0;
+
+for(let i = 1; ; i++){
+    if(i >= 5){
+        price = 750;
+    }
+    if(wallet >= price){
+        wallet -= price;
+        count++;
+        console.log(i + "回目の買い物成功。残金は" + wallet + "円");
+    }
+    else{
+        console.log("買い物終了" + count + "個買えた");
+        break;
+    }
+}
