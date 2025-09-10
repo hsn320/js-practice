014963f102d5c052aa51987c1da605ec4db70dbd
hsn320 <2250025@ecc.ac.jp>
2025-09-10 16:18:26 +0900

    dom04

---
+++ 05_DOM/js/dom04.js
@@ -0,0 +1,7 @@
+const school = document.getElementById("school");
+const btn = document.getElementById("btn");
+const text = document.getElementById("text");
+
+btn.onclick = () => {
+    text.innerText = school.value;
+}
