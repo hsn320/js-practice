3368d3a9a7fea3533579bc86d2bcd80f1fbfbe2a
hsn320 <2250025@ecc.ac.jp>
2025-09-10 12:08:07 +0900

    array04

---
+++ 04_array/js/array04.js
@@ -0,0 +1,12 @@
+let adobe = ["Photoshop","Illustrator","FireWorks"];
+console.log(adobe);
+
+//配列adobeの末尾を削除
+adobe.pop();
+console.log(adobe);
+
+let ecccomp = ["GameCreative","IT","Design"];
+console.log(ecccomp);
+
+ecccomp.pop();
+console.log(ecccomp);
