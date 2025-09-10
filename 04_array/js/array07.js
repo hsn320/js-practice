50a5043bd50182196362b6120289baa17bbe48cd
hsn320 <2250025@ecc.ac.jp>
2025-09-10 12:34:23 +0900

    array07

---
+++ 04_array/js/array07.js
@@ -0,0 +1,19 @@
+let adobe = ["Photoshop","Illustrator","XD"];
+
+console.log(adobe);
+
+//1番目の要素を１つ削除
+adobe.splice(1,1);
+console.log(adobe);
+
+//1番目にFigmaを追加
+adobe.splice(1,0,"Figma");
+console.log(adobe);
+
+
+
+let menu = ["Aランチ","Bランチ","モーニング定食","パスタ定食"]
+console.log(menu);
+
+menu.splice(2,1);
+console.log(menu);
