987057717336f8c1151d28b1ad6a02006b1b087c
hsn320 <2250025@ecc.ac.jp>
2025-09-12 23:10:42 +0900

    timer03

--- 08_timer/js/timer03.js
+++ 08_timer/js/timer03.js
@@ -1,1 +1,13 @@
 const arr = ["HTML","CSS","JavaScript","Pug","Sass","TypeScript"];
+const skill = document.getElementById("skill");
+let count = 0;
+
+const timer = setInterval(()=>{
+    if(count < arr.length){
+        skill.innerHTML += `<li>${arr[count]}</li>`;
+        count++
+    }
+    else{
+        return false;
+    }
+},3000);
