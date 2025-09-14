3b8f5de40cafe4c293735aff2d69e1d347c50c77
hsn320 <2250025@ecc.ac.jp>
2025-09-14 15:11:35 +0900

    swtich01

---
+++ practice/02_if/js/swtich01.js
@@ -0,0 +1,15 @@
+const code = "HTML";
+
+switch(code){
+    case "HTML":
+        console.log(`拡張子は${code}です`);
+        break;
+    case "CSS":
+        console.log(`拡張子は${code}です`);
+        break;
+    case "JavaScript":
+        console.log(`拡張子は${code}です`);
+        break;
+    default:
+        console.log("わからないファイルです");
+}
