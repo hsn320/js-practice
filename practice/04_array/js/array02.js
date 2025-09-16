3465df5ef704027cb1c5e37f17bd1b3509203137
hsn320 <2250025@ecc.ac.jp>
2025-09-16 23:23:54 +0900

    array02

---
+++ practice/04_array/js/array02.js
@@ -0,0 +1,12 @@
+let ramen = [];
+
+ramen[0] = "sio";
+ramen[1] = "miso";
+ramen[2] = "tonkotu";
+ramen[3] = "nibosi";
+ramen[4] = "shoyu";
+ramen[5] = "jirou";
+
+console.log(ramen[0],ramen[1],ramen[2],ramen[3],ramen[4],ramen[5]);
+
+console.log(`ここのお店のラーメンは全部で${ramen.length}種類、おすすめは${ramen[1]}です`);
