0308357e47c44e6145ec83f0900551b4890973ce
hsn320 <2250025@ecc.ac.jp>
2025-09-20 14:41:24 +0900

    loop01

--- practice/07_loop/js/loop01.js
+++ practice/07_loop/js/loop01.js
@@ -5,4 +5,14 @@ const menu = [
   "ステーキ定食",
   "カツカレー定食",
 ];
+const Menu = document.getElementById("menu");
 
+menu.forEach( e => {
+  const li = document.createElement("li");
+  li.textContent = e;
+  Menu.appendChild(li);
+});
+
+// createElement　jsで新しいhtml要素を作る
+// ↪︎タグを書いても文字として表示される
+// textContent html要素の中にあるテキストを読み取ったり書き換えたりできるプロパティ
