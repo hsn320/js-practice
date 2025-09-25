06eada1caeceffc74c813f0550e39bac4e69d82a
hsn320 <2250025@ecc.ac.jp>
2025-09-25 22:57:01 +0900

    if03

---
+++ practice2/02_if/js/if03.js
@@ -0,0 +1,13 @@
+const wallet = 3000;
+let item = prompt();
+item = Number(item);
+
+if(wallet > item){
+    console.log("購入することができます");
+}
+else if(wallet == item){
+    console.log("ピッタリです");
+}
+else{
+    console.log("購入することができません");
+}
