ea79e57ce264d19de897299bb2e8d28706bdc38f
hsn320 <2250025@ecc.ac.jp>
2025-09-26 16:48:39 +0900

    dom08

---
+++ practice2/05_DOM/js/dom08.js
@@ -0,0 +1,10 @@
+const L = document.getElementById("btnL");
+const S = document.getElementById("btnS");
+const text = document.getElementById("text");
+
+L.onclick = () =>{
+    text.style.fontSize =  "80px";
+}
+S.onclick = () =>{
+    text.style.fontSize = "20px";
+}
