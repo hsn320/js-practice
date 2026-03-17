ad623d843332f5d76967f79c019bda111fc3eee6
hsn320 <2250025@ecc.ac.jp>
2026-03-17 11:17:00 +0900

    switch01,02

---
+++ practice4/02_if/js/swtich01.js
@@ -0,0 +1,19 @@
+let code = "HTML";
+
+switch(code){
+    case "HTML":
+        console.log("拡張子は.htmlです");
+        break;
+
+    case "CSS":
+        console.log("拡張子は.cssです");
+        break;
+
+    case "JavaScript":
+        console.log("拡張子は.cssです");
+        break;
+
+    default:
+        console.log("分からないファイルです");
+        break;
+}
