d1f388008ddd60585de072eeb625da0cefe8e8b4
hsn320 <2250025@ecc.ac.jp>
2026-04-08 10:58:58 +0900

    function06

--- practice4/06_function/js/function06_practice.js
+++ practice4/06_function/js/function06_practice.js
@@ -1,16 +1,27 @@
-const btnA = document.getElementById("btnA");
-const btnB = document.getElementById("btnB");
-const btnC = document.getElementById("btnC");
+// const btnA = document.getElementById("btnA");
+// const btnB = document.getElementById("btnB");
+// const btnC = document.getElementById("btnC");
+// const result = document.getElementById("result");
+
+// btnA.onclick = () => {
+//   result.innerText = "btnAが押された";
+// }
+
+// btnB.onclick = () => {
+//   result.innerText = "btnBが押された";
+// }
+
+// btnC.onclick = () => {
+//   result.innerText = "btnCが押された";
+// }
+
+const btn = document.getElementsByClassName("btn");
 const result = document.getElementById("result");
 
-btnA.onclick = () => {
-  result.innerText = "btnAが押された";
+function msg(e){
+  result.innerText = `${e.target.innerText}が押された`;
 }
 
-btnB.onclick = () => {
-  result.innerText = "btnBが押された";
-}
-
-btnC.onclick = () => {
-  result.innerText = "btnCが押された";
+for (let i=0; i<btn.length; i++){
+  btn[i].onclick = msg;
 }
